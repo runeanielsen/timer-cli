c87cbd6b9e4259d985032e7d1258cec06635061d
Rune Nielsen <runenielsen@runbox.com>
2022-11-27 16:47:52 +0100

    cancel now has a simpler implementation

--- src/cancel.rs
+++ src/cancel.rs
@@ -7,9 +7,8 @@ pub fn cancel(time_entry_path: &PathBuf) -> Result<(), &'static str> {
     if Path::new(time_entry_path).exists() {
         fs::remove_file(time_entry_path).unwrap();
         println!("Timer has now been canceled.");
+        Ok(())
     } else {
-        Err("Cannot cancel, no running timer.")?;
+        Err("Cannot cancel, no running timer.")
     }
-
-    Ok(())
 }
