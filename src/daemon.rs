e72c42eab336d0272c8dc92ce5ec1aa1e7cccff2
Rune Nielsen <runenielsen@runbox.com>
2022-12-17 23:29:55 +0100

    now does a double `fork()` to ensure we are not the session leader

--- src/daemon.rs
+++ src/daemon.rs
@@ -7,20 +7,32 @@ unsafe fn _daemonize() -> Result<(), &'static str> {
         -1 => return Err("Fork failed."),
         // The child process becomes the daemon, and can now continue running as normal.
         0 => {}
-        // The parent process can exit, as the daemon will continue running in the child process.
+        // The parent process can exit,
+        // as the daemon will continue running in the child process.
+        _ => libc::_exit(0),
+    };
+
+    // Create a new session for the daemon,
+    // if the value returned is -1, the session could not be created.
+    if setsid() == -1 {
+        return Err("Could not set sid.");
+    }
+
+    // Ensure that we are not the session leader.
+    match fork() {
+        -1 => return Err("Fork failed."),
+        // The child process becomes the daemon, and can now continue running as normal.
+        0 => {}
+        // The parent process can exit,
+        // as the daemon will continue running in the child process.
         _ => libc::_exit(0),
     };
 
     // We want to use the system-wide default file-mode for the daemon.
     umask(0);
 
-    // Create a new session for the daemon,
-    // if the value returned is less than 0, the session could not be created.
-    if setsid() < 0 {
-        return Err("Could not set sid.");
-    }
-
-    // The daemon does not require a specific.
+    // The current directory was inherited from the parent process,
+    // to reset it to 'default', we switch to the root directory.
     if set_current_dir("/").is_err() {
         return Err("Could not set current directory.");
     };
